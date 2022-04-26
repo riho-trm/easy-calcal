5ef9dbdf1fbfb1f174be47c95c916b8304c94d66
riho-trm <riho.n.0304+1@gmail.com>
2022-04-26 17:46:34 +0900

    ボタンコンポーネントのファイル作成

---
+++ src/frontend/components/presentational/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent ({
+setup(){
+    const
+}
+});
+</script>
+
+<style></style>
