e02673dad45dfb2c0719dfb47f975d8fbdc0b44e
riho-trm <riho.n.0304+1@gmail.com>
2022-04-27 16:48:13 +0900

    buttonのhtml部分と挙動を作成

--- src/frontend/components/presentational/BaseButton.vue
+++ src/frontend/components/presentational/BaseButton.vue
@@ -1,15 +1,64 @@
 <template>
-  <div></div>
+  <div>
+    <button type="button" class="btn btn--orange btn--radius" @click="clicked">
+      {{ buttonText }}
+    </button>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
 
-export default defineComponent ({
-setup(){
-    const
-}
+export default defineComponent({
+  props: {
+    buttonText: {
+      type: String,
+      default: "PUSH",
+    },
+  },
+  setup(props, context) {
+    const clicked = () => {
+      context.emit("button-clicked");
+    };
+  },
 });
 </script>
 
-<style lang="scss"></style>
+<style scoped lang="scss">
+*,
+*:before,
+*:after {
+  -webkit-box-sizing: inherit;
+  box-sizing: inherit;
+}
+
+html {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-size: 62.5%; /*rem算出をしやすくするために*/
+}
+
+.btn,
+a.btn,
+button.btn {
+  font-size: 1.6rem;
+  font-weight: 700;
+  line-height: 1.5;
+  position: relative;
+  display: inline-block;
+  padding: 1rem 4rem;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
+  letter-spacing: 0.1em;
+  color: #212529;
+  border-radius: 0.5rem;
+}
+</style>
