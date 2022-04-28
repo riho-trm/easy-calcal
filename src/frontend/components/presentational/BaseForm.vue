203e1fd82f59434206aa4961614e533e870f25ce
riho-trm <riho.n.0304+1@gmail.com>
2022-04-28 16:04:12 +0900

    フォームのバインディング処理を追加

---
+++ src/frontend/components/presentational/BaseForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    type="text"
+    class="input-form"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+// 親コンポーネントに埋める際はinputのvalueを格納する変数を用意し、v-modelを設定する
+export default defineComponent({
+  props: ["modelValue"],
+  emits: ["update:modelValue"],
+});
+</script>
+
+<style lang="scss" scoped>
+// 仮CSS
+.input-form {
+  border: 0;
+  padding: 10px;
+  font-size: 1.3em;
+  font-family: Arial, sans-serif;
+  color: #aaa;
+  border: solid 1px #ccc;
+  margin: 0 0 20px;
+  width: 300px;
+}
+</style>
--- src/frontend/views/HomeView.vue
+++ src/frontend/views/HomeView.vue
@@ -3,21 +3,28 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
     <app-routing-button buttonText="押してみよう！" routing="about" />
+    <BaseForm v-model="message" />
+    <div>{{ message }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import BaseButton from "@/components/presentational/BaseButton.vue";
+// import BaseButton from "@/components/presentational/BaseButton.vue";
 import AppRoutingButton from "@/components/container/AppRoutingButton.vue";
+import BaseForm from "@/components/presentational/BaseForm.vue";
+import { defineComponent, ref } from "vue";
 
-@Options({
+export default defineComponent({
   components: {
     HelloWorld,
-    BaseButton,
+    // BaseButton,
     AppRoutingButton,
+    BaseForm,
   },
-})
-export default class HomeView extends Vue {}
+  setup() {
+    let message = ref("");
+    return { message };
+  },
+});
 </script>
