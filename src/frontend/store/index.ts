67d73e37ff2294d85b3bd8f6a2415f799daec595
riho-trm <riho.n.0304+1@gmail.com>
2022-05-20 09:46:06 +0900

    sessionstrageの追加

--- src/frontend/store/index.ts
+++ src/frontend/store/index.ts
@@ -1,14 +1,18 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+  plugins: [
+    createPersistedState({
+      // ストレージのキーを指定
+      key: "vuex",
+      // ストレージの種類を指定
+      storage: window.sessionStorage,
+    }),
+  ],
+});
