4daf7c0d0a0edc7468b18f6988ae9b8c0d179360
riho-trm <riho.n.0304+1@gmail.com>
2022-05-23 16:07:21 +0900

    デザイン実装

--- src/frontend/views/Login.vue
+++ src/frontend/views/Login.vue
@@ -3,64 +3,79 @@
     <div class="title-animation">
       <h1 class="title-logo">title（仮）</h1>
       <img
+        class="humberger-img"
         src="../assets/hamburger-g53f2dd67d_640.jpg"
         alt="ハンバーガーの写真"
       />
-      <img src="../assets/muffins-g1f7348331_640.jpg" alt="マフィンの写真" />
+      <img
+        class="img muffin-img"
+        src="../assets/muffins-g1f7348331_640.jpg"
+        alt="マフィンの写真"
+      />
     </div>
-    <div id="login">
-      <h2>ログイン</h2>
-      <div class="user-id">
-        <BaseInput
-          id="email-input"
-          name="email-input"
-          type="email"
-          placeholder="メールアドレス"
-          v-model="state.email"
+    <div class="login-wrapper">
+      <div class="login">
+        <h2>ログイン</h2>
+        <div class="email">
+          <BaseInput
+            id="email-input"
+            name="email-input"
+            type="email"
+            placeholder="メールアドレス"
+            v-model="state.email"
+          />
+          <div
+            class="input-errors"
+            v-for="(error, index) in v$.email.$errors"
+            :key="index"
+          >
+            <div class="error-msg" v-if="error.$validator == 'required'">
+              メールアドレスを入力してください
+            </div>
+            <div class="error-msg" v-else-if="error.$validator == 'email'">
+              メールアドレスの形式が不正です
+            </div>
+          </div>
+          <div class="error-msg">
+            {{ resErrorMessage.email }}
+          </div>
+        </div>
+
+        <div class="password">
+          <BaseInput
+            id="password-input"
+            name="password"
+            type="password"
+            placeholder="パスワード"
+            v-model="state.password"
+          />
+          <div
+            class="input-errors"
+            v-for="(error, index) in v$.password.$errors"
+            :key="index"
+          >
+            <div class="error-msg" v-if="error.$validator === 'required'">
+              パスワードを入力してください
+            </div>
+            <div class="error-msg" v-if="error.$validator === 'fixedPassword'">
+              大小半角英数字混在で8文字以上のパスワードを入力してください
+            </div>
+          </div>
+          <div class="error-msg">
+            {{ resErrorMessage.password }}
+          </div>
+        </div>
+        <AppProcessingButton
+          class="btn login-btn"
+          buttonText="ログイン"
+          @processing="validateTest"
         />
-        <div
-          class="input-errors"
-          v-for="(error, index) in v$.email.$errors"
-          :key="index"
-        >
-          <div class="error-msg" v-if="error.$validator == 'required'">
-            メールアドレスを入力してください
-          </div>
-          <div class="error-msg" v-else-if="error.$validator == 'email'">
-            メールアドレスの形式が不正です
-          </div>
-        </div>
-        <div class="error-msg">
-          {{ resErrorMessage.email }}
-        </div>
-      </div>
-      <div class="password">
-        <BaseInput
-          id="password-input"
-          name="password"
-          type="password"
-          placeholder="パスワード"
-          v-model="state.password"
+        <hr />
+        <AppProcessingButton
+          class="btn register-btn"
+          buttonText="新規登録はこちら"
         />
-        <div
-          class="input-errors"
-          v-for="(error, index) in v$.password.$errors"
-          :key="index"
-        >
-          <div class="error-msg" v-if="error.$validator === 'required'">
-            パスワードを入力してください
-          </div>
-          <div class="error-msg" v-if="error.$validator === 'fixedPassword'">
-            大小半角英数字混在で8文字以上のパスワードを入力してください
-          </div>
-        </div>
-        <div class="error-msg">
-          {{ resErrorMessage.password }}
-        </div>
       </div>
-      <AppProcessingButton buttonText="ログイン" @processing="validateTest" />
-      <hr />
-      <AppProcessingButton buttonText="新規登録はこちら" />
     </div>
   </div>
 </template>
@@ -146,7 +161,79 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.error-msg {
-  color: red;
+.container {
+  display: flex;
+  width: 90%;
+  margin: 0 auto;
+  height: 80vh;
+  .title-animation {
+    width: 50%;
+    margin-left: auto;
+    position: relative;
+    .title-logo {
+      font-size: 3rem;
+      padding: 30px 0;
+    }
+    img {
+      width: 400px;
+    }
+    .muffin-img {
+      position: absolute;
+      top: 280px;
+      right: 25px;
+    }
+  }
+
+  .login-wrapper {
+    width: 50%;
+    margin-right: auto;
+    border: solid;
+    border-color: #dfdfdf;
+    position: relative;
+    .login {
+      width: 90%;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translateY(-50%) translateX(-50%);
+      -webkit-transform: translateY(-50%) translateX(-50%);
+      h2 {
+        padding: 20px 0;
+        font-size: 2rem;
+      }
+      :deep(.input-form) {
+        width: 50%;
+        height: 35px;
+        border: 1px solid #dcdcdc;
+      }
+      .email,
+      .password {
+        margin-bottom: 35px;
+      }
+      .error-msg {
+        color: red;
+      }
+      .btn {
+        padding: 0;
+        :deep(.btn) {
+          font-size: 1rem;
+          padding: 0.5rem 2rem;
+        }
+      }
+      .login-btn {
+        margin-bottom: 35px;
+      }
+      .register-btn {
+        margin: 35px 0;
+      }
+      hr {
+        height: 1px;
+        width: 80%;
+        margin: 0 auto;
+        background-color: #dcdcdc;
+        border: none;
+      }
+    }
+  }
 }
 </style>
