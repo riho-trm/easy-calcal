f51f471892cd54789f4d1a9995f143d4c19c150c
riho-trm <riho.n.0304+1@gmail.com>
2022-06-10 14:02:48 +0900

    topページのルーティング修正

--- src/frontend/router/index.ts
+++ src/frontend/router/index.ts
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import Login from "../views/Login.vue";
-// import LoginTest from "../views/LoginTest.vue";
-import TopView from "../views/TopView.vue";
 import MembershipRegistration from "../views/MembershipRegistration.vue";
 import TopMenu from "../views/TopMenu.vue";
 import AdminMenu from "../views/AdminMenu.vue";
@@ -16,39 +13,21 @@ import { authorizeToken } from "./guards";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: HomeView,
+    name: "TopMenu",
+    component: TopMenu,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
   },
-  // {
-  //   path: "/logintest",
-  //   name: "loginTest",
-  //   component: LoginTest,
-  // },
-  {
-    path: "/topview",
-    name: "TopView",
-    component: TopView,
-    meta: {
-      requiresAuth: true,
-    },
-  },
   {
     path: "/register",
     name: "Register",
     component: MembershipRegistration,
-  },
-  {
-    path: "/topmenu",
-    name: "TopMenu",
-    component: TopMenu,
-    meta: {
-      requiresAuth: true,
-    },
   },
   {
     path: "/adminmenu",
