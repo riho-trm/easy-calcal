622b8aa96d7cb30272f77ae3bc43417c775813f7
riho-trm <riho.n.0304+1@gmail.com>
2022-06-13 14:12:31 +0900

    ストアから取得したデータを表示する処理

--- src/frontend/views/MyDataList.vue
+++ src/frontend/views/MyDataList.vue
@@ -8,11 +8,11 @@
           name="my-data-search"
           type="text"
           placeholder="Myデータを検索"
-          v-model="state.nutrientsListFoodName"
+          v-model="searchWord"
         />
         <!-- </div>
-        <div class="search-btn"> -->n
-        <AppProcessingButton buttonText="検索" @processing="validateTest" />
+        <div class="search-btn"> -->
+        <AppProcessingButton buttonText="検索" />
         <!-- </div> -->
       </div>
       <div class="list-wrapper">
@@ -22,10 +22,10 @@
             <th>タイトル</th>
             <th>更新日時</th>
           </tr>
-          <tr class="my-data">
-            <td>テスト1</td>
-            <td>テスト2</td>
-            <td>テスト3</td>
+          <tr class="my-data" v-for="state in states" :key="state.savedDataId">
+            <td>{{ state.savedDataId }}</td>
+            <td>{{ state.title }}</td>
+            <td>{{ state.updatedAt }}</td>
           </tr>
         </table>
       </div>
@@ -37,8 +37,8 @@
 <script lang="ts">
 import AppProcessingButton from "@/components/container/AppProcessingButton.vue";
 import BaseInput from "@/components/presentational/BaseInput.vue";
-import axios from "axios";
-import { defineComponent } from "vue";
+import { MyData } from "@/types/task";
+import { defineComponent, reactive, ref } from "vue";
 import { useStore } from "vuex";
 
 export default defineComponent({
@@ -46,15 +46,20 @@ export default defineComponent({
   setup() {
     const store = useStore();
 
+    let states = reactive([]) as MyData[];
+    const searchWord = ref("");
+
     const created = async () => {
       try {
-        await store.dispatch("getmydatalist");
+        await store.dispatch("getmydata");
+        const res = store.getters.getMyDataList;
+        res.forEach((data: MyData) => states.push(data));
       } catch (error) {
         console.log(error);
       }
     };
     created();
-    return {};
+    return { states, searchWord };
   },
 });
 </script>
