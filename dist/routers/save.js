eec84b44e6616854ae2bc250a7fa1e4c79504680
riho-trm <riho.n.0304+1@gmail.com>
2022-06-22 13:53:12 +0900

    ConnectionPoolに変更

--- dist/routers/save.js
+++ dist/routers/save.js
@@ -8,7 +8,7 @@ const mysql_1 = __importDefault(require("mysql"));
 const authenticate_1 = require("../middlewares/authenticate");
 const router = express_1.default.Router();
 router.use(express_1.default.json());
-const connection = mysql_1.default.createConnection({
+const pool = mysql_1.default.createPool({
     host: "us-cdbr-east-05.cleardb.net",
     user: "be3a5ee1ceb501",
     password: "224a4fb1",
@@ -16,69 +16,90 @@ const connection = mysql_1.default.createConnection({
 });
 router.post("/savemydata", authenticate_1.authenticate, (req, res) => {
     const sql = "INSERT INTO saved_data (user_id, title, memo, url) VALUES (?,?,?,?)";
-    connection.query(sql, [req.body.userId, req.body.title, req.body.memo, req.body.url], (err, result) => {
-        if (err)
-            throw err;
-        res.json({ status: "success", insertId: result.insertId });
+    pool.getConnection((err, connection) => {
+        connection.query(sql, [req.body.userId, req.body.title, req.body.memo, req.body.url], (err, result) => {
+            if (err)
+                throw err;
+            res.json({ status: "success", insertId: result.insertId });
+            connection.release();
+        });
     });
 });
 router.post("/savemynutrients", authenticate_1.authenticate, (req, res) => {
     const sql = 
     // 配列でinsertするときのvaluesは?ひとつのみ
     "INSERT INTO saved_nutrients (saved_data_id, nutrient_id, quantity) VALUES ?";
-    connection.query(sql, [req.body], (err, result) => {
-        if (err)
-            throw err;
-        res.json({ status: "success", res: result });
+    pool.getConnection((err, connection) => {
+        connection.query(sql, [req.body], (err, result) => {
+            if (err)
+                throw err;
+            res.json({ status: "success", res: result });
+            connection.release();
+        });
     });
 });
 router.get("/getmydata", authenticate_1.authenticate, (req, res) => {
     const sql = "SELECT * FROM saved_data WHERE user_id=?";
     // getメソッドで絞り込み条件を渡す際にはqueryに渡す
-    connection.query(sql, [req.query.userId], (err, result) => {
-        if (err)
-            throw err;
-        res.json({ status: "success", myData: result });
+    pool.getConnection((err, connection) => {
+        connection.query(sql, [req.query.userId], (err, result) => {
+            if (err)
+                throw err;
+            res.json({ status: "success", myData: result });
+            connection.release();
+        });
     });
 });
 router.get("/getmynutrients", authenticate_1.authenticate, (req, res) => {
     const sql = "SELECT * FROM saved_nutrients WHERE saved_data_id in (?)";
-    connection.query(sql, [req.query.savedDataId], (err, result) => {
-        if (err)
-            throw err;
-        res.json({ status: "success", myNutrients: result });
+    pool.getConnection((err, connection) => {
+        connection.query(sql, [req.query.savedDataId], (err, result) => {
+            if (err)
+                throw err;
+            res.json({ status: "success", myNutrients: result });
+            connection.release();
+        });
     });
 });
 // ok
 router.put("/updatesaveddata", authenticate_1.authenticate, (req, res) => {
     const sql = "update saved_data set title=?, memo=?, url=? WHERE id=?";
-    connection.query(sql, [req.body.title, req.body.memo, req.body.url, req.body.savedDataId], function (err, result) {
-        if (err) {
-            throw err;
-        }
-        res.json({ status: "success" });
+    pool.getConnection((err, connection) => {
+        connection.query(sql, [req.body.title, req.body.memo, req.body.url, req.body.savedDataId], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.json({ status: "success" });
+            connection.release();
+        });
     });
 });
 // ok
 router.put("/updatesavednutrients", authenticate_1.authenticate, (req, res) => {
     console.log(req.body);
     const sql = "INSERT INTO saved_nutrients (id, saved_data_id, nutrient_id, quantity) VALUES ? ON DUPLICATE KEY UPDATE quantity=VALUES(quantity)";
-    connection.query(sql, [req.body.editedData], function (err, result) {
-        if (err) {
-            throw err;
-        }
-        res.json({ status: "success" });
+    pool.getConnection((err, connection) => {
+        connection.query(sql, [req.body.editedData], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.json({ status: "success" });
+            connection.release();
+        });
     });
 });
 // ok
 router.delete("/deletesavednutrients", authenticate_1.authenticate, (req, res) => {
     console.log(req.body);
     const sql = "delete from saved_nutrients WHERE id in (?)";
-    connection.query(sql, [req.body.savedNutrientsId], function (err, result) {
-        if (err) {
-            throw err;
-        }
-        res.json({ status: "success" });
+    pool.getConnection((err, connection) => {
+        connection.query(sql, [req.body.savedNutrientsId], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.json({ status: "success" });
+            connection.release();
+        });
     });
 });
 // ok
@@ -86,22 +107,27 @@ router.delete("/deletemydata", authenticate_1.authenticate, (req, res) => {
     const sevedDataSql = "delete from saved_data WHERE id=?";
     const savedNutrientsSql = "delete from saved_nutrients WHERE saved_data_id=?";
     console.log(req.body.savedDataId);
-    connection.query(savedNutrientsSql, [req.body.savedDataId], function (err, result) {
-        if (err) {
-            throw err;
-        }
+    pool.getConnection((err, connection) => {
+        connection.query(savedNutrientsSql, [req.body.savedDataId], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            connection.query(sevedDataSql, [req.body.savedDataId], function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                connection.release();
+            });
+            res.json({ status: "success" });
+            connection.release();
+        });
         connection.query(sevedDataSql, [req.body.savedDataId], function (err, result) {
             if (err) {
                 throw err;
             }
+            res.json({ status: "success" });
+            connection.release();
         });
-        res.json({ status: "success" });
-    });
-    connection.query(sevedDataSql, [req.body.savedDataId], function (err, result) {
-        if (err) {
-            throw err;
-        }
-        res.json({ status: "success" });
     });
 });
 exports.default = router;
